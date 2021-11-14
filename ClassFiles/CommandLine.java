cea11ec09b3adc61a5cb6340f26b60dafaf96869
shubham488-creator <73779227+shubham488-creator@users.noreply.github.com>
2021-11-14 12:54:54 +0530

    14/11/2021

---
+++ ClassFiles/CommandLine.java
@@ -0,0 +1,15 @@
+
+//DISPLAY ALL COMMAND-LINE ARGUMENTS
+
+class CommandLine
+{
+ public static void main(String args[])
+ {
+  for(int i=0;i<args.length;i++)
+  {
+   System.out.println("args["+i+"]: "+args[i]);
+  }
+  
+ }
+}
+   
---
+++ ClassFiles/StringArrays.java
@@ -0,0 +1,17 @@
+
+//DEMONSTRATING STRING ARRAYS
+
+class StringDemo3
+{
+ public static void main(String args[])
+ {
+  String str[]={"one","two","three"};
+  
+  for(int i=0;i<str.length;i++)
+     System.out.println("Str["+i+"]: "+str[i]);
+     
+ }
+ 
+}
+
+  
