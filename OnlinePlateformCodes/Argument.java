41e1ee1dc3eaac1dadc9a0b42baa1dc3f181367d
shubham488-creator <73779227+shubham488-creator@users.noreply.github.com>
2022-02-01 14:12:15 +0530

    Add files via upload

---
+++ OnlinePlateformCodes/Argument.java
@@ -0,0 +1,20 @@
+
+// SIMPLE CODE TO KNOW THE USE OF STRING ARGS[].
+
+class Argument
+{
+ public static void main(String[] args)
+ {
+  
+  //Running a for loop
+  
+  for(String str:args)
+  {
+   System.out.println(str);
+  }
+  
+ }
+ 
+}
+
+   
