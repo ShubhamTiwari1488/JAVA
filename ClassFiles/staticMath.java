ccd4d6930dbaaecd038768a269f3469449efc671
SHUBHAM TIWARI <73779227+shubham488-creator@users.noreply.github.com>
2022-04-05 10:08:33 +0530

    Add files via upload

---
+++ ClassFiles/staticMath.java
@@ -0,0 +1,14 @@
+import static java.lang.Math.*;
+
+class Main
+{
+ public static void main(String args[])
+ {
+  System.out.println("2^3 is = "+pow(2,3));
+  
+  System.out.println("Sqrt of 2 is : "+sqrt(2));
+  
+  System.out.println("Random number is : "+(random()*100));
+ }
+}
+  
