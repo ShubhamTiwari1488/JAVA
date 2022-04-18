1b2bd667a5f7d14df3b8a4900aaaca2432347d80
SHUBHAM TIWARI <73779227+shubham488-creator@users.noreply.github.com>
2022-04-18 16:30:26 +0530

    Update Static_Class.java

--- ClassFiles/Static_Class.java
+++ ClassFiles/Static_Class.java
@@ -1,9 +1,10 @@
+
 class OuterClass {
 
-	
+	// Input string
 	private static String msg = "GeeksForGeeks";
 
-	
+	// Static nested class
 	private static void show()
 	{
 	    System.out.println("Hello");
@@ -13,11 +14,11 @@ class OuterClass {
 	{
 	    System.out.println("Shubham");
 	}
-	
-	public static class NestedStaticClass 
-	{
+	public static class NestedStaticClass {
 
-		
+		// Only static members of Outer class
+		// is directly accessible in nested
+		// static class
 		public void printMessage()
 		{
             OuterClass sh = new OuterClass();
@@ -28,21 +29,40 @@ class OuterClass {
 			show();
 			
 			sh.display();
-	
+			//display();
 		}
 	}
 
+	// Non-static nested class -
+	// also called Inner class
+	public class InnerClass {
+
+		// Both static and non-static members
+		// of Outer class are accessible in
+		// this Inner class
+		public void display()
+		{
+
+			// Print statement whenever this method is
+			// called
+			System.out.println(
+				"Message from non-static nested class: "
+				+ msg);
+		}
+	}
 }
 
 
-class Main
-{
-    public static void main(String args[])
+class Main {
+
+
+	public static void main(String args[])
 	{
 
 		// Creating instance of nested Static class
 		// inside main() method
-		OuterClass.NestedStaticClass printer = new OuterClass.NestedStaticClass();
+		OuterClass.NestedStaticClass printer
+			= new OuterClass.NestedStaticClass();
 
 		// Calling non-static method of nested
 		// static class
@@ -54,15 +74,16 @@ class Main
 		// Creating Outer class instance for creating
 		// non-static nested class
 		OuterClass outer = new OuterClass();
-		
-		OuterClass.InnerClass inner	= outer.new InnerClass();
+		OuterClass.InnerClass inner
+			= outer.new InnerClass();
 
 		// Calling non-static method of Inner class
 		inner.display();
 
 		// We can also combine above steps in one
 		// step to create instance of Inner class
-		OuterClass.InnerClass innerObject = new OuterClass().new InnerClass();
+		OuterClass.InnerClass innerObject
+			= new OuterClass().new InnerClass();
 
 		// Similarly calling inner class defined method
 		innerObject.display();
